ac22134c52964bce9ce72786d97addb5dfb8d641
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-09-19 18:06:02 -0400

    8

--- beginner.js
+++ beginner.js
@@ -342,8 +342,8 @@ function simpleMultiplication(number) {
 
 // FOR TWINS TYPES
 function typeValidation(variable, type) {
-  // Your code should be here ;) 
-  return typeof variable === type ?  true : false;
+  // Your code should be here ;)
+  return typeof variable === type ? true : false;
 }
 
 // THE WIDE-MOUTHED FROG
@@ -358,182 +358,178 @@ function mouthSize(animal) {
   // return newAni === "alligator" ? "small" : "wide"
 }
 
-// REDUCE BUT GROW 
-function grow(x){
-  return x.reduce((prev, curr) => prev * curr);                           
+// REDUCE BUT GROW
+function grow(x) {
+  return x.reduce((prev, curr) => prev * curr);
 }
 
 // FILLING ARRAY PART 1
 const arr = (n) => {
   let newArray = [];
-  for(let i = 0; i < n; i++) {
-    newArray.push(i)
+  for (let i = 0; i < n; i++) {
+    newArray.push(i);
   }
   return newArray;
-}
+};
 
 // POWER OF 2
-function powersOfTwo(n){
+function powersOfTwo(n) {
   let newArr = [];
-   for (let i = 0; i <= n; i++) {
-     newArr[i] = 2**i ;
-   }
-   return newArr;
- }
-
- // REMOVE AN EXCLAMATION MARK FROM THE END OF A STRING
- function remove (string) {
-  // using regex
-//   For the $ quantifier it will search Exclamation mark at the end of the string.
-// Tip: Use the ^n quantifier to match any string with n at the BEGINNING of it.
-  return string.replace(/!$/, "")
+  for (let i = 0; i <= n; i++) {
+    newArr[i] = 2 ** i;
+  }
+  return newArr;
 }
 
-// WILSON PRIMES 
-FACTORIALS 
+// REMOVE AN EXCLAMATION MARK FROM THE END OF A STRING
+function remove(string) {
+  // using regex
+  //   For the $ quantifier it will search Exclamation mark at the end of the string.
+  // Tip: Use the ^n quantifier to match any string with n at the BEGINNING of it.
+  return string.replace(/!$/, "");
+}
+
+// WILSON PRIMES
+FACTORIALS;
 function amIWilson(p) {
   // check if prime is Wilson
   // return (Array.from({length: p -1}, (_, index) => index +1).reduce((prev,curr) => prev * curr, 1) + 1) / (p * p) % 1 == 0
   const factorial = (a) => {
-    return a <= 1 ? 1 : a * factorial(a -1)
-  }
-  return (factorial(p - 1) + 1) / (p * p) % 1 === 0;
+    return a <= 1 ? 1 : a * factorial(a - 1);
+  };
+  return ((factorial(p - 1) + 1) / (p * p)) % 1 === 0;
 
-//   const factorial = (a) => {
-//     return a <= 1 ? 1 : a * factorial(a -1)
-//   }
-//  if (((factorial(p - 1) + 1) % (p * p)) == 0) {
-//    return true;
-//  } else {
-//    return fals
+  //   const factorial = (a) => {
+  //     return a <= 1 ? 1 : a * factorial(a -1)
+  //   }
+  //  if (((factorial(p - 1) + 1) % (p * p)) == 0) {
+  //    return true;
+  //  } else {
+  //    return fals
 }
 
-// COUNT ODD NUMBERS BELOW N 
-function oddCount(n){
+// COUNT ODD NUMBERS BELOW N
+function oddCount(n) {
   // your code here
   let newArr = [];
   for (let i = 0; i < n; i++) {
     if (i % 2 != 0) {
-      newArr.push(i)
+      newArr.push(i);
     }
   }
   return newArr.length;
-};
+}
 
-// REMOVE EXCLAMATION MARK 
+// REMOVE EXCLAMATION MARK
 function removeExclamationMarks(s) {
-  return s.replace(/!/g, "")
-};
+  return s.replace(/!/g, "");
+}
 
-// OPPOSITE NUMBER 
+// OPPOSITE NUMBER
 function opposite(number) {
   //your code here
-  return - number;
-};
+  return -number;
+}
 
 // SAFEN USER INPUT PART 1- HTMLINSPECIALCHARS
 function htmlspecialchars(formData) {
   // Insert your code here
-  return formData.replace(/&/g, "&amp;")
-                 .replace(/</g, "&lt;")
-                 .replace(/"/g, "&quot;")
-                 .replace(/>/g, "&gt;");
-  
-};
+  return formData
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/"/g, "&quot;")
+    .replace(/>/g, "&gt;");
+}
 
 // ADD ITEM TO AN ARRAY
-websites.push("codewars")
+websites.push("codewars");
 
 // MR.FREEZE
 // mark the MrFreeze object instance as frozen
 Object.freeze(MrFreeze);
 
-// CONVERT A STRING INTO AN ARRAY 
-function stringToArray(string){
-
-	// code code code
-  return string.split(" ")
-
-};
+// CONVERT A STRING INTO AN ARRAY
+function stringToArray(string) {
+  // code code code
+  return string.split(" ");
+}
 
 // COUNT BY X
 function countBy(x, n) {
   let z = [];
-  for(let i = 1; i <= n; i++) {
-       z.push(i * x)
+  for (let i = 1; i <= n; i++) {
+    z.push(i * x);
   }
   return z;
-};
+}
 
 // CVS REPRESENTATION OF ARRAY
 function toCsvText(array) {
   // good luck
- return array.join("\n")
-};
+  return array.join("\n");
+}
 
 // CONVERT NUUMBER TO REVERSED ARRAY OF DIGITS
 function digitize(n) {
   //code here
-  console.log(n)
+  console.log(n);
   let arr = n.toString().split("");
   return arr.map(Number).reverse();
-};
+}
 
 // BEGINNER SERIES #2 CLOCK
-function past(h, m, s){
+function past(h, m, s) {
   //#Happy Coding! ^_^
-  console.log(h, m, s)
-  return new Date().setHours(h,m,s) - new Date().setHours(0,0,0);
-//   Date
-//   JavaScript Date objects represent a single moment in time in a platform-independent format. Date objects contain a Number that represents milliseconds since 1 January 1970 UTC.
+  console.log(h, m, s);
+  return new Date().setHours(h, m, s) - new Date().setHours(0, 0, 0);
+  //   Date
+  //   JavaScript Date objects represent a single moment in time in a platform-independent format. Date objects contain a Number that represents milliseconds since 1 January 1970 UTC.
   // new Date()
-//   When called as a constructor, returns a new Date object.
-};
+  //   When called as a constructor, returns a new Date object.
+}
 
 // BIN TO DECIMAL
-function binToDec(bin){
+function binToDec(bin) {
   // ...
   return parseInt(bin, 2);
-};
+}
 
 // CENTURY FROM YEAR
 function century(year) {
   // Finish this :)
   // The Math.ceil() function always rounds up and returns the smaller integer greater than or equal to a given number.
-  console.log(year)
-  return Math.ceil(year / 100)
-};
+  console.log(year);
+  return Math.ceil(year / 100);
+}
 
 // WHAT IS BETWEEN
 function between(a, b) {
   // your code here
-  console.log(a,b);
+  console.log(a, b);
   let newArr = [];
-  for(let i = a; i <= b; i++) {
+  for (let i = a; i <= b; i++) {
     newArr.push(i);
   }
   return newArr;
 }
 
 // FIND NUMBERS WHIICH ARE DIVISIBLE BY GIVEN NUMBER
-function divisibleBy(numbers, divisor){
+function divisibleBy(numbers, divisor) {
   let newArr = [];
   for (let i = 0; i <= numbers.length; i++) {
-    if(numbers[i] % divisor === 0) {
-      newArr.push(numbers[i])
+    if (numbers[i] % divisor === 0) {
+      newArr.push(numbers[i]);
     }
   }
   return newArr;
-
 }
 
-// SHORT LONG SHORT 
-function solution(a, b){
+// SHORT LONG SHORT
+function solution(a, b) {
   // your code here
   if (a.length < b.length) {
     return a + b + a;
-  }
-  else {
+  } else {
     return b + a + b;
   }
 }
@@ -543,40 +539,39 @@ function findMultiples(integer, limit) {
   //your code here
   let final = [];
   for (let i = integer; i <= limit; i += integer) {
-    final.push(i)
-    }
-return final;
+    final.push(i);
+  }
+  return final;
 }
 
 // SUM OF POSITIVES
 function positiveSum(arr) {
-  console.log(arr)
+  console.log(arr);
   let total = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] >= 0) {
-      total += arr[i]
+      total += arr[i];
     }
   }
   return total;
 }
 
-// NAME SHUFFLER 
-function nameShuffler(str){
+// NAME SHUFFLER
+function nameShuffler(str) {
   //Shuffle It
-  console.log(str)
+  console.log(str);
   // destructuring
-  let [firstName, lastName] = str.split(' ');
+  let [firstName, lastName] = str.split(" ");
   return lastName + " " + firstName;
-  
 }
 
 // INVERT VALUES
 function invert(array) {
-  let newArr = []
+  let newArr = [];
   for (let i = 0; i < array.length; i++) {
     if (array[i] >= 0 || array[i] < 0) {
-      newArr.push(array[i] * -1)
-    } 
+      newArr.push(array[i] * -1);
+    }
   }
   return newArr;
 
@@ -584,24 +579,50 @@ function invert(array) {
 }
 
 // REVERSED SEQUENCE
-const reverseSeq = n => {
+const reverseSeq = (n) => {
   let finalRes = [];
-   for (let i = n; i > 0; i--) {
-     finalRes.push(i)
-   }
-   return finalRes;
- };
+  for (let i = n; i > 0; i--) {
+    finalRes.push(i);
+  }
+  return finalRes;
+};
 
- // MAKE UPPERCASE
- function makeUpperCase(str) {
+// MAKE UPPERCASE
+function makeUpperCase(str) {
   // Code here
-  return str.toUpperCase()
+  return str.toUpperCase();
 }
 
 // CONVERT A NUMBER TO A STRING
 function numberToString(num) {
   // Return a string of the number here!
-  return num.toString()
+  return num.toString();
 }
 
+// LOST WITHOUT A MAP
+function maps(x) {
+  return x.map((s) => s * 2);
+}
 
+// RETURN NEGATIVE
+function makeNegative(num) {
+  // Code?
+  return num * -1
+}
+
+// RETURN NEGATIVE
+function makeNegative(num) {
+  // The Math.abs() function returns the absolute value of a number.
+  console.log(num)
+  return Math.abs(num) * - 1
+}
+
+// CONVERT A BOOLEAN TO A STRING 
+function booleanToString(b){
+  //   if (b){
+  //     return "true"
+  //   }else {
+  //     return "false"
+  //   }
+    return b ? "true" : "false"
+  }
