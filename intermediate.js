186d5d3295e8ccca88248ccab11e6976de71c74f
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-09-25 09:21:18 -0400

    some intermediates

--- intermediate.js
+++ intermediate.js
@@ -28,8 +28,47 @@ function createPhoneNumber(numbers) {
   return phoneNumFormat;
 }
 
-// STOP gninnipS My sdroW! 
-function spinWords(string){
+// STOP gninnipS My sdroW!
+function spinWords(string) {
   //TODO Have fun :)
-return string.length >= 5 ? string.split(" ").map((letter) => (letter.length >= 5 ? letter.split("").reverse().join("") : letter)).join(" ") : string;
-  }
+  return string.length >= 5
+    ? string
+        .split(" ")
+        .map((letter) =>
+          letter.length >= 5 ? letter.split("").reverse().join("") : letter
+        )
+        .join(" ")
+    : string;
+}
+
+// FILTER OUT THE GEESE
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+  // return an array containing all of the strings in the input array except those that match strings in geese
+  return birds.filter((bird) => !geese.includes(bird));
+}
+
+// ENUMERABLE MAGIC TRUE FOR ALL
+
+function all( arr, fun ){
+  // The some() method tests whether at least one element in the array passes the test implemented by the provided function. It returns true if, in the array, it finds an element for which the provided function returns true; otherwise it returns false. It doesn't modify the array.The some() method tests whether at least one element in the array passes the test implemented by the provided function. It returns true if, in the array, it finds an element for which the provided function returns true; otherwise it returns false. It doesn't modify the array.
+
+  // The every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value
+ 
+  // return arr.some(fun)
+  return arr.every(fun)
+}
+
+// ENUMERABLE MAGIC TRUE FOR NONE ?
+function none(arr, fun){
+  // ...
+//   console.log(arr, fun)
+//   for(let i = 0; i < arr.length; i++){
+//     if(fun(arr[i])){
+//       return false;
+//     } 
+//     return true;
+//   }
+  return !arr.some(fun)
+}
