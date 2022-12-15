6865f40ae46914a7b84a5f101c85a9ee4b72a539
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-12-15 16:52:09 -0500

    basics started

---
+++ JavaScriptBasics.js
@@ -0,0 +1,55 @@
+// PRIMITIVE TYPES IN JAVASCRIPT
+Number
+String
+Boolean
+null
+undefined
+
+// R E P L => read  evaluate print loop
+
+// NUMBERS
+positive
+negative
+whole 
+decimal
+
+// P E M D A S => paranthesis exponents multiplication division addition substraction
+
+NaN
+// the intentional abscence of a number. or a numeric value that represents something that is...not a number
+
+// VARIABLES => are like labels for values. we can store a value and give it a name so that we can:
+// 1. refer back to it later  2. use that value to do stuff  3. or change it later on
+example 
+// using the LET keyword
+let year = 1991;
+// make me a variable called "year" and give it the value of 1991
+let  numHens = 6;
+let numRoosters = 2;
+console.log(numHens + numRoosters)
+let totalChicken = numHens + numRoosters;
+console.log(totalChicken);
+
+let myScore = 0;
+myScore += 5;
+console.log(myScore);
+
+// using the CONST keyword
+// const works just like let. except you  cannot change the value
+
+String =>
+// STRINGS => are just another primitive types in javaScript. they represt text and must be wrapped in quotes.
+example 
+let userName = "Adnan";
+console.log(userName)
+// strings are indexed, each character has a corresponding index (a positional number)
+// eg: C H I C K E N
+//     0 1 2 3 4 5 6
+// String concatenation
+let firstName = "Adnan";
+let lastName = "Adams";
+let fullName = firstName + " " + lastName;
+
+// STRING METHODS => methods are built-in actions we can perform with individual strings
+// they help us do things like ==> searching within a string, replacing part of a string, changing the casing of a string
+
