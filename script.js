be48fd9291e2682a5cb16ec8bbaccd2a820fbe6e
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-12-21 23:27:59 -0500

    switch

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CONDITIONALS
 console.log("its me again");
 console.log("BEFORE CONDITIONAL");
 let randomNum = Math.random();
@@ -29,25 +30,57 @@ console.log("AFTER CONDITIONAL");
 const age = 82;
 
 if (age < 5) {
-    console.log("you are a baby. you get in for free")
+    // console.log("you are a baby. you get in for free")
 } else if (age < 10) {
-    console.log("you are a child. You pay $10")
+    // console.log("you are a child. You pay $10")
 }  else if (age < 65) {
-    console.log("you are an adult. You pay $20")
+    // console.log("you are an adult. You pay $20")
 } else {
-    console.log("You are a senior, You pay $10")
+    // console.log("You are a senior, You pay $10")
 }
 
-const password = prompt("please enter a new password");
+// const password = prompt("please enter a new password");
 
 // pass must be 6+ characters
 if (password.length >= 6) {
     // password cannot include space
 if (password.indexOf(" ") === -1) {
-    console.log("Valid Password!");
+    // console.log("Valid Password!");
 } else {
-    console.log("Password cannot contain spaces!")
+    // console.log("Password cannot contain spaces!")
 }
 } else {
-    console.log("PASSWORD TOO SHORT! Must be 6+ characters")
+    // console.log("PASSWORD TOO SHORT! Must be 6+ characters");
 }
+
+// LOGICAL OPERATORS 
+
+// && AND => Both sides must be true, for the entire thing to be true;
+1 <= 4 && "a" === "a"; // true
+9 > 10 && 9 >= 9; // false
+"abc".length === 3 && 1+1 === 4; // false
+if (password.length >= 6 && password.indexOf(" ") === -1) {
+    console.log("Valid Password");
+} else {
+    console.log("Invalid Password");
+}
+
+// || OR => if one side is true, the entire thing is true, only one side needs to be true
+1 !== 1 || 10 === 10 // true
+10/2 === 5 || null // true
+0 || undefined // false
+const ageOf = -10;
+if (ageOf >= 0 && ageOf < 5 || ageOf >= 65) {
+    console.log("FREE");
+} else if (ageOf >= 5 && ageOf < 10) {
+    console.log("$10");
+} else if (ageOf >= 10 && ageOf < 65) {
+    console.log("$20");
+} else {
+    console.log("INVALID AGE");
+}
+
+// ! NOT => ! expression returns true if expression is false
+!null // true
+! (0 === 0) // false
+! (3 <= 4) // false
