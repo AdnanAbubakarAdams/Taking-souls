cdd278d0da660ce0e0580ce177016498eed60d87
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2023-01-11 17:38:59 -0500

    second problem on leetcode

--- LeetCode.js
+++ LeetCode.js
@@ -1,10 +1,38 @@
-// 832. FLIPPING AN IMAGE 
-var flipAndInvertImage = function(image) {
-    for (let i = 0; i < image.length; i++) {
-        image[i].reverse();
-        image[i] = image[i].map(img => img === 0 ? 1 : 0)
-    }
-    return image;
+// 832. FLIPPING AN IMAGE
+var flipAndInvertImage = function (image) {
+  for (let i = 0; i < image.length; i++) {
+    image[i].reverse();
+    image[i] = image[i].map((img) => (img === 0 ? 1 : 0));
+  }
+  return image;
 };
 
 // 56. MERGE INTERVALS
+var merge = function (intervals) {
+  //edge case: theres only 1 interval
+  if (intervals.length === 1) return intervals;
+  // sort intervals based on start time
+  intervals.sort((a, b) => a[0] - b[0]);
+  //declare a variable for start value of 1st interval
+  let start = intervals[0][0];
+  // declare a variable for tracking end value of 1st interval
+  let end = intervals[0][1];
+  // declare a variable to push the result into
+  let mergedArr = [];
+  // loop through the intervals starting at index 1 (2nd interval)
+  for (let i = 1; i < intervals.length; i++) {
+    // declare a variable for the current index
+    const currentInterval = intervals[i];
+    // compare the curr ind to end
+    if (currentInterval[0] <= end) {
+      end = Math.max(currentInterval[1], end);
+    } else {
+      mergedArr.push([start, end]);
+      start = currentInterval[0];
+      end = currentInterval[1];
+    }
+  }
+  mergedArr.push([start, end]);
+
+  return mergedArr;
+};
