ca68d905d5b47220551944059f7bb79b8159bd43
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2023-01-19 22:35:40 -0500

    leetcode :(

--- LeetCode.js
+++ LeetCode.js
@@ -62,3 +62,10 @@ var numberOfSteps = function(num) {
       return 1 + steps;
   }
 };
+
+// 509. FIBONACCI NUMBER
+var fib = function(n) {
+  if (n <= 0) return 0;
+  if (n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2)
+};
