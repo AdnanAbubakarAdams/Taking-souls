470b1806aa96dc074865a62718a5ea6fa800ebde
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-07-11 23:53:42 -0400

    birthdayProblem

--- practice-box/LeetCode.js
+++ practice-box/LeetCode.js
@@ -1540,6 +1540,30 @@ var longestConsecutive = function(nums) {
   return maxCount;
 };
 
+var climbStairs = function(n) {
+
+  if (n < 2) {
+
+      return 1;
+  }
+
+  let firstStep = 1;
+
+  let secondStep = 1;
+
+  let thirdStep = 0;
+
+  for (let i = 2; i <= n; i++) {
+
+      thirdStep = firstStep + secondStep;
+
+      firstStep = secondStep;
+
+      secondStep = thirdStep;
+  }
+
+  return thirdStep;
+};
 
 
 // using a class for function above
