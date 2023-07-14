2ada50159150e620131372487ff8545f1c018edb
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-07-14 11:41:18 -0400

    morningRoutine

--- practice-box/LeetCode.js
+++ practice-box/LeetCode.js
@@ -1540,6 +1540,16 @@ var longestConsecutive = function(nums) {
   return maxCount;
 };
 
+// 70. Climbing Stairs
+// You are climbing a staircase. It takes n steps to reach the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+// Example 1:
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+
 var climbStairs = function(n) {
 
   if (n < 2) {
@@ -1563,6 +1573,30 @@ var climbStairs = function(n) {
   }
 
   return thirdStep;
+};
+
+// 2469. Convert the Temperature
+// You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+// You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+// Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+// Note that:
+// Kelvin = Celsius + 273.15
+// Fahrenheit = Celsius * 1.80 + 32.00
+// Example 1:
+// Input: celsius = 36.50
+// Output: [309.65000,97.70000]
+// Explanation: Temperature at 36.50 Celsius converted in Kelvin is 309.65 and converted in Fahrenheit is 97.70.
+
+var convertTemperature = function(celsius) {
+  let temperatureArr = [];
+
+  let convertedToKelvin = celsius + 273.15;
+
+  let convertedToFahrenheit = celsius * 1.80 + 32.00;
+
+  temperatureArr.push(convertedToKelvin, convertedToFahrenheit);
+
+  return temperatureArr;
 };
 
 
