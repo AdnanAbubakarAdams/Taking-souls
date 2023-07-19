f8e813c7b43532ba22ee5f1064e5a157dd8472ef
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-07-19 16:17:23 -0400

    daily dose of sql and javascript basics

--- practice-box/Oreilly/SqlIn10.sql
+++ practice-box/Oreilly/SqlIn10.sql
@@ -1,40 +1,102 @@
--- SQL - STRUCTURED QUERY LANGUAGE
+SQL - STRUCTURED QUERY LANGUAGE
 
 -- DATABASE 
--- Is where structured data is stored
--- -- Think of a database as a filing cabinet that contains lots of drawers and folders
--- Databases are managed by Database Management System software(DBMS)
--- --EXAMPLES => DBMS's are DB2, MySQL, Microsoft SQL Server, Oracle, PostgresSQL   
+Is where structured data is stored
+Think of a database as a filing cabinet that contains lots of drawers and folders
+Databases are managed by Database Management System software(DBMS)
+-- EXAMPLES DBMS's are DB2, MySQL, Microsoft SQL Server, Oracle, PostgresSQL   
 
 --TABLES 
--- A table is wkere data of a specific type is stored
--- -- if the file cabinet is the database, file folders can be thought of as the tables
--- Tables are very structured => they can only contain data as specified in the table description
--- -- A table set up to store product list couldnt store customer info
+A table is wkere data of a specific type is stored
+if the file cabinet is the database, file folders can be thought of as the tables
+Tables are very structured => they can only contain data as specified in the table description
+A table set up to store product list couldnt store customer info
 
 -- COLUMNS
--- Tables are made up of columns that contain specific parts of the data (product name, product price, contact firstname, contact lastname, etc)
+Tables are made up of columns that contain specific parts of the data (product name, product price, contact firstname, contact lastname, etc)
 
--- When a table is created, each column is defined with: 
-    -- A name
-    -- A datatype (which defines what type of data the column will accept)
+When a table is created, each column is defined with: 
+    A name
+    A datatype (which defines what type of data the column will accept)
 
 -- ROWS 
-    -- Rows are the data records
-    -- Rows can be thought of as the individual pages in the file folder
--- When a record is added to a table, a row is added, and data is stored in each column for the new row
+    Rows are the data records
+    Rows can be thought of as the individual pages in the file folder
+When a record is added to a table, a row is added, and data is stored in each column for the new row
 
 -- RELATIONAL DATABASE
--- A relational database is a data management system that stores data in lots of specific tables that are connected to each other
--- The connections are called relationships
--- SQL is the language used to work with relational data
+A relational database is a data management system that stores data in lots of specific tables that are connected to each other
+The connections are called relationships
+SQL is the language used to work with relational data
 
 -- KEYS 
--- For relational databases to work, tow important keys are needed:
-    -- Every table should have a Primary key, the value of which uniquely identifies each and every row
-    -- Foreign keys are used in related tables to connect the data back to the primary key in the primary table
+For relational databases to work, tow important keys are needed:
+    Every table should have a Primary key, the value of which uniquely identifies each and every row
+    Foreign keys are used in related tables to connect the data back to the primary key in the primary table
 
 -- DBMS fall into two broad categories
-    -- client-server: MariaDB, Microsoft SQL server, MySQL, Oracle, PostgresSQL
-    -- Shared-file: Access, FileMaker
+    client-server: MariaDB, Microsoft SQL server, MySQL, Oracle, PostgresSQL
+    Shared-file: Access, FileMaker
+
+    -- RETRIEVING DATA
+-- USE THE SELECT STATEMENT
+SELECT is used to select (retrieve) data from a database table
+SELECT requires two pieces of information
+SELECT 
+    what to SELECT 
+    where to SELECT it from
+-- could be very simple SELECT column FROM table;
+eg: SELECT * FROM Products; 
+eg: SELECT prod_name FROM Products;
+eg: SELECT prod_name, prod_id, prod_price FROM Products; will retrieve ddta according to your arrangement of colums
+
+-- RETRIEVE DISTINCT DATA OR ROWS (to eliminate duplicates)
+    SELECT DISTINCT vend_id FROM Products;
+
+-- LIMIT RETRIEVE DATA
+    SELECT prod_name FROM Products LIMIT 5;
+
+-- NB; -- use whitespace
+    whitespace makes code more readable;
+        white space is ignored by the DBMS
+        Use indentation, line breaks, alignment
+
+        -- TERMINATION
+    Best practices dictate that all  SQL statement be terminated with a  ;
+        Usually not absolutely necessary, but use is highly recommended
+    
+        -- CASE INSENSITIVE 
+    As general rule case does not matter in SQL
+        Depending on how your DBMS is configured, case may indeed matter for object names (table names, column names, etc.)
+        Pick a case convention and stick with it
+
+    -- USE COMMENTS
+        Use comments to: 
+            Describe your SQL
+            Prevent code from being executed ("commenting out")
+
+-- SORTING RETRIEVED DATA
+-- USE ORDER BY
+    -- To specify sort order, add ORDER BY to your select statement
+    -- ORDER BY requirres one piece of information
+        -- the column to sort by
+        -- optionally, you may also specify multiple sort colums as well as sort direction
+    SELECT column FROM table ORDER BY column [ASC | DESC];
+    SELECT prod_name FROM Products ORDER BY prod_name; -- this order prod_name alphabetically
+    SELECT prod_name FROM Products ORDER BY prod_id; -- will order by id note that your order by any column in your table whether you are using it in your retrieval or not.
+
+    -- SORT BY MULTIPLE COLUMNS
+    SELECT prod_id, prod_name, prod_price FROM Products ORDER BY prod_price, prod_name; -- they sorted by price and alphabetical order
+
+    SELECT prod_id, prod_name, prod_price FROM Products ORDER BY prod_price DESC; -- sorted by the most expensive to the least expensive
+
+    NB: 
+    Use SELECT ORDER BY to sort retrieved data 
+    You can sort by one or more rows/columns
+    Sort order can be specified for each ORDER BY row, default is ASC(ascending order)
+    Best practices: 
+        Never rely on default sort order, always provide an explicit sort order
+        Avoid using relative column positions in ORDER BY
+
+    --  FILTERING DATA
     
--- practice-box/intro.js
+++ practice-box/intro.js
@@ -41,13 +41,67 @@
   console.log(roundNumsDownToTheNearestFive([3,33,19,6])) // [0, 3, 15, 5]
   
   // 6. Given a list of words, return a count of the number of words that start or end with a vowel
+  const isVowel = (chars) => {
+    let vowels = ["a", "e", "i", "o", "u"];
+
+    return vowels.includes(chars.toLowerCase());
+  }
+  const countWordsThatStartOrEndWithAVowel = (words) => {
+    let count = 0;
+
+    for (let word of words) {
+      let firstChar = word[0];
+      let lastChar = word[word.length - 1];
+
+      if (isVowel(firstChar) || isVowel(lastChar)) {
+        count ++;
+      }
+    }
+
+    return count;
+  };
+  console.log(countWordsThatStartOrEndWithAVowel(["on", "let"])) // 1
   
   // 7. given an array of words, return a string made of the first character of each word
+  const makeAStringFromAllFirstCharInAnArray = (words) => {
+    let strOfAllFirstChar = [];
+    
+    // for (let i = 0; i < words.length; i++) {
+
+    //   let firstChar = words[i][0];
+
+    //   strOfAllFirstChar.push(firstChar);
+    // }
+    for (let word of words) {
+
+      let firstChar = word[0];
+
+      strOfAllFirstChar.push(firstChar)
+    }
   
+    return strOfAllFirstChar.join("");
+  }
+  console.log(makeAStringFromAllFirstCharInAnArray(['pop', 'lopez', 'odor', 'pug']));
   // ['pop', 'lopez', 'odor', 'pug'] => 'plop'
   
   
   // 8. given an array with words, numbers and objects, return the count of how many objects there are
+  const getCountOfObjectsInAnArray = (arr) => {
+    let count = 0;
+
+    for (let element of arr) {
+      if (typeof element === "object") {
+
+        count++
+
+      }
+    }
+
+    return count;
+  }
+  console.log(getCountOfObjectsInAnArray( [])) // 0
+  console.log(getCountOfObjectsInAnArray( [{}, 4, 'dog'])) // 1
+  console.log(getCountOfObjectsInAnArray(['dad', 23, 1983, {'h': 3}, 'girl', {2: 34}])) // 2
   
   // ['dad', 23, 1983, {'h': 3}, 'girl', {2: 34}] => 2
   // [{}, 4, 'dog'] => 1
@@ -55,21 +109,42 @@
   
   
   // 9. given a string, return the string  wrapped in a span element
-  
   // 'Hello World' => '<span>Hello World</span>'
   // 'Yo!' => '<span>Yo!</span>'
-  
   // '<span>' + 'Hellow worlkd!' + '</span>'
+  function makeAstringWrappedInASpan (str) {
+    return `<span> ${str} </span>`
+  }
+  console.log(makeAstringWrappedInASpan('Yo!'))
+  console.log(makeAstringWrappedInASpan('Hellow worlkd!'))
+  
   
   // 10. given two numbers, price and discount, return the price after the discount is applied
-  
   // price is 100 and discount is 15%
   // 100, 15 => 85
-  
   // 70, 3 => 67.9
+  const getThePriceOfItemAfterDiscountIsApplied = (price, discount) => {
+  
+
+    let discountedPrice = (discount / 100) * price;
+
+    // return Math.round(price - discountedPrice);
+    return price - discountedPrice;
+  }
+  console.log(getThePriceOfItemAfterDiscountIsApplied(100, 15))
+  console.log(getThePriceOfItemAfterDiscountIsApplied(70, 3))
   
   
   // 11. given a string with two words, put ' the ' between the words and return the string
-  
   // 'to beach' => 'to the beach'
-  // 'for real' => 'for the real'
+  // 'for real' => 'for the real one'
+
+  const putTheInBetweenTwoStrs = (str) => {
+    let splittedStr = str.split(" ").join(" the ")
+    // let madeStr = splittedStr.join(" the ");
+
+    // return madeStr;
+    return splittedStr;
+  }
+  console.log(putTheInBetweenTwoStrs('to beach'))
+  console.log(putTheInBetweenTwoStrs('for real-one'))
