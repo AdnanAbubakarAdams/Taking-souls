d709ae5bdc42923109525b239534cc65a58a26c1
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-07-21 23:25:06 -0400

    weekendwelit

--- practice-box/LeetCode.js
+++ practice-box/LeetCode.js
@@ -1549,7 +1549,6 @@ var longestConsecutive = function(nums) {
 // Explanation: There are two ways to climb to the top.
 // 1. 1 step + 1 step
 // 2. 2 steps
-
 var climbStairs = function(n) {
 
   if (n < 2) {
@@ -1599,6 +1598,25 @@ var convertTemperature = function(celsius) {
   return temperatureArr;
 };
 
+// 771. Jewels and Stones
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+// Input: jewels = "z", stones = "ZZ"
+// Output: 0
+var numJewelsInStones = function(jewels, stones) {
+  let countOfStones = 0;
+  
+  for(let i = 0; i < stones.length; i++) {
+      
+      if (jewels.includes(stones[i])){
+
+          countOfStones++
+      }
+  }
+
+  return countOfStones
+};
 
 // using a class for function above
 // class Counter {
