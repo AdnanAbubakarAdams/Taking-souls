dec2aa06df6ff66e0f1c507c8789454f1c0e7456
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-08-02 23:58:44 -0400

    leetnight

--- practice-box/Matrixes.js
+++ practice-box/Matrixes.js
@@ -48,24 +48,27 @@ const findMaxValueInMatrix = () => {
 // console.log(findMaxValueInMatrix(), "im here");
 
 // 3. find the sum of each row
-// const sumOfRows = (matrix) => {
-//     const sumOfRowOne = 0;
-//     const sumOfRowTwo = 0;
-//     const sumOfRowThree = 0;
-//     const sumOfRowFour = 0;
+const sumOfRows = (matrix) => {
 
-//     for(let i = 0; i < matrix.length; i++) {
-//         for(let j = 0; j < matrix[i].length; j++) {
-//             // sumOfRowOne = matrix[i][j];
-//             sumOfRowOne = matrix[i][j] + matrix[i][j + 1] + matrix[i][j + 2] + matrix[i][j + 3]
-//         }
-//     }
-//     return sumOfRowOne;
-// }
-// console.log(sumOfRows([[0, 2, 4, 5],
-//     [9, 0, 8, 1],
-//     [0, 0, 9, 1],
-//     [3, 5, 9, 2]]))
+    let results = [];
+    const sumOfRowOne = matrix[0][0] + matrix[0][1] + matrix[0][2] + matrix[0][3];
+    const sumOfRowTwo = matrix[1][0] + matrix[1][1] + matrix[1][2] + matrix[1][3];
+    const sumOfRowThree = matrix[2][0] + matrix[2][1] + matrix[2][2] + matrix[2][3];
+    const sumOfRowFour = matrix[3][0] + matrix[3][1] + matrix[3][2] + matrix[3][3];
+
+    // for(let i = 0; i < matrix.length; i++) {
+    //     for(let j = 0; j < matrix[i].length; j++) {
+    //         // sumOfRowOne = matrix[i][j];
+    //         sumOfRowOne = matrix[i][j] + matrix[i][j + 1] + matrix[i][j + 2] + matrix[i][j + 3]
+    //     }
+    // }
+    results.push(sumOfRowOne, sumOfRowTwo, sumOfRowThree, sumOfRowFour);
+    return results;
+}
+console.log(sumOfRows([[0, 2, 4, 5],
+    [9, 0, 8, 1],
+    [0, 0, 9, 1],
+    [3, 5, 9, 2]]))
 
 // 4. find the greatest sum of any two horizontally consecutive numbers
 
