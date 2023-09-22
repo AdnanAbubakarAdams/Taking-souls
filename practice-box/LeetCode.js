aa3035c90dbd3b7920f8df710cfe9edf56a32bfb
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-09-21 22:56:02 -0400

    codewarsLeetcode

--- practice-box/LeetCode.js
+++ practice-box/LeetCode.js
@@ -2179,6 +2179,58 @@ var findDisappearedNumbers = function(nums) {
   return disappearedNumsFound;
 };
 
+// 961. N-Repeated Element in Size 2N Array
+// You are given an integer array nums with the following properties:
+// nums.length == 2 * n.
+// nums contains n + 1 unique elements.
+// Exactly one element of nums is repeated n times.
+// Return the element that is repeated n times.
+// Example 1:
+// Input: nums = [1,2,3,3]
+// Output: 3
+// Example 2:
+// Input: nums = [2,1,2,5,3,2]
+// Output: 2
+// Example 3:
+// Input: nums = [5,1,5,2,5,3,5,4]
+// Output: 5
+var repeatedNTimes = function(nums) {
+  let mostFreqObj = {};
+  let maxElement = nums[0];
+  let maxCountOfElement = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] in mostFreqObj) {
+          mostFreqObj[nums[i]]++;
+      } else {
+          mostFreqObj[nums[i]] = 1;
+      }
+
+      if (mostFreqObj[nums[i]] > maxCountOfElement) {
+          maxElement = nums[i];
+          maxCountOfElement = mostFreqObj[nums[i]]
+      }
+  }
+
+  return maxElement;
+};
+
+// 977. Squares of a Sorted Array
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+// Example 1:
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+// Explanation: After squaring, the array becomes [16,1,0,9,100].
+// After sorting, it becomes [0,1,9,16,100].
+// Example 2:
+// Input: nums = [-7,-3,2,3,11]
+// Output: [4,9,9,49,121]
+var sortedSquares = function(nums) {
+  return nums.map((element) => element ** 2).sort((a, b) => a - b)
+};
+
+
+
 
 
 // using a class for function above
--- practice-box/intermediate.js
+++ practice-box/intermediate.js
@@ -676,3 +676,22 @@ function descendingOrder(n){
 const arrCheck = (value) => {
   return value.every((val) => Array.isArray(val));
 } 
+
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+function sortArray(array) {
+  // Return a sorted array.
+  let arrOfOddNums = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 !== 0) {
+      arrOfOddNums.push(array[i])
+    }
+  }
+  arrOfOddNums.sort((a, b) => a - b);
+  
+  return array.map((element) => element % 2 ? arrOfOddNums.shift() : element)
+}
