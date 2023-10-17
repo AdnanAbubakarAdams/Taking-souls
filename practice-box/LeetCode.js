920d11b2e2de8e1ea69db68671cc6f5620f5cdee
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-10-16 22:03:26 -0400

    leetcodes

--- practice-box/LeetCode.js
+++ practice-box/LeetCode.js
@@ -2248,6 +2248,67 @@ var rotate = function(matrix) {
   return reversedMatrix;
 };
 
+// 442. Find All Duplicates in an Array
+// Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+// You must write an algorithm that runs in O(n) time and uses only constant extra space.
+// Example 1:
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [2,3]
+// Example 2:
+// Input: nums = [1,1,2]
+// Output: [1]
+// Example 3:
+// Input: nums = [1]
+// Output: []
+
+var findDuplicates = function(nums) {
+  nums.sort()
+  let duplicates = [];
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === nums[i+1]) {
+    duplicates.push(nums[i])
+  }
+}
+return duplicates
+};
+
+// 1365. How Many Numbers Are Smaller Than the Current Number
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+// Return the answer in an array.
+// Example 1:
+// Input: nums = [8,1,2,2,3]
+// Output: [4,0,1,1,3]
+// Explanation: 
+// For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+// For nums[1]=1 does not exist any smaller number than it.
+// For nums[2]=2 there exist one smaller number than it (1). 
+// For nums[3]=2 there exist one smaller number than it (1). 
+// For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+// Example 2:
+// Input: nums = [6,5,4,8]
+// Output: [2,1,0,3]
+// Example 3:
+// Input: nums = [7,7,7,7]
+// Output: [0,0,0,0]
+var smallerNumbersThanCurrent = function(nums) {
+  let numberThatAreSmaller = [];
+
+  for(let i = 0; i < nums.length; i++) {
+      let count = 0;
+      for (let j = 0; j < nums.length; j++) {
+          if (nums[j] < nums[i]) {
+              count++;
+          }
+      }
+
+      numberThatAreSmaller.push(count)
+  }
+
+  return numberThatAreSmaller;
+};
+
+
 
 
 
